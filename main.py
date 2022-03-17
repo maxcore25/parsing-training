d550fb9adcd674607913c2beffbc5d64dc8bb44a
maxcore25 <mkorobov2002@mail.ru>
2022-03-17 19:33:17 +0300

    Selenium Function Added And Requirements Updated

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
-from pprint import pprint
-import time
-import json
+from selenium import webdriver
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def get_first_news():
+def use_bs():
     headers = {
         'user-agent': os.getenv('USER_AGENT')
     }
     url = os.getenv('URL')
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, 'lxml')
-    article_cards = soup.find_all('a', class_='article-card')
+    # soup = BeautifulSoup(r.text, 'lxml')
+    # article_cards = soup.find_all('a', class_='article-card')
+
+
+def use_selenium():
+    browser = webdriver.Chrome()
+    browser.get(os.getenv('URL'))
+
+    xpath = '/html/body/div[1]/div[1]/div[1]/dl/dd/div[7]/div[1]/div/button'
+    browser.find_element_by_xpath(xpath).click()
 
 
 def main():
-    get_first_news()
+    # use_bs()
+    use_selenium()
 
 
 if __name__ == '__main__':
